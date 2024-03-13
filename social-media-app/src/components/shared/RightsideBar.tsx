ee63966f61354d61c5c6de784c76716b6f7ee2d3
Diego Engelhard <00356119@uca.edu.sv>
2024-03-13 11:41:32 -0600

    style: right side bar layout

--- social-media-app/src/components/shared/RightsideBar.tsx
+++ social-media-app/src/components/shared/RightsideBar.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const RightsideBar = () => {
     return (
-        <div>RightsideBar</div>
+        <section className='custom-scrollbar rightsidebar'>
+            {/* Display Suggested communities */}
+            <div className='flex flex-1 flex-col justify-start'>
+                <h3 className='text-heading4-medium text-light-1'>
+                    Suggested Communities
+                </h3>
+            </div>
+
+            {/* Display Suggested Users */}
+            <div className='flex flex-1 flex-col justify-start'>
+                <h3 className='text-heading4-medium text-light-1'>
+                    Suggested Users
+                </h3>
+            </div>
+        </section>
     )
 }
 

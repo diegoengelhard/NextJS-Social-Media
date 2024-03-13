3d617d031ae1cd21db54f406e1776716d31db508
Diego Engelhard <00356119@uca.edu.sv>
2024-03-13 10:37:16 -0600

    style: app design layout

--- social-media-app/src/app/layout.tsx
+++ social-media-app/src/app/layout.tsx
@@ -3,6 +3,11 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import TopBar from '@/components/shared/Topbar';
+import Bottombar from '@/components/shared/Bottombar';
+import LeftsideBar from '@/components/shared/LeftsideBar';
+import RightsideBar from '@/components/shared/RightsideBar';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,7 +23,20 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+        <body className={`${inter.className} bg-dark-1`}>
+          <TopBar />
+
+          <main className='flex flex-row'>
+            <LeftsideBar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightsideBar />
+          </main>
+
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   )

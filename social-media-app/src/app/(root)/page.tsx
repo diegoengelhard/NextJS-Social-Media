5a51bf82c42faad0b14190829fd317241a449536
Diego Engelhard <00356119@uca.edu.sv>
2024-03-15 12:51:52 -0600

    feat: fetch posts

--- social-media-app/src/app/(root)/page.tsx
+++ social-media-app/src/app/(root)/page.tsx
@@ -1,10 +1,42 @@
 import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
+import { fetchPosts } from "@/lib/actions/posts.actions";
 
-export default function Home() {
+import PostCard from "@/components/cards/PostCard";
+
+export default async function Home() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const result = await fetchPosts();
+
   return (
     <main className="text-white">
       <h1 className="text-4xl font-bold text-white">Welcome to the social media app</h1>
       <UserButton afterSignOutUrl="/" />
+
+      {/* Render fetched posts */}
+      <section className='mt-9 flex flex-col gap-10'>
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No posts found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <PostCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </main>
   )
 }
--- social-media-app/src/lib/models/Post.model.ts
+++ social-media-app/src/lib/models/Post.model.ts
@@ -24,7 +24,7 @@ const postSchema = new mongoose.Schema({
     children: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: "Thread",
+            ref: "Post",
         },
     ],
 }, { timestamps: true });
